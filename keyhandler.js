8ef4f0739f755870f3384c0d2263b21293989740
Mayson1337 <99101632+Mayson1337@users.noreply.github.com>
2023-03-15 23:30:06 +0100

    Delete keyhandler.js

--- keyhandler.js
+++
@@ -1,9 +0,0 @@
-mp.keys.bind(0x45, true, () => {
-  mp.events.callRemote('server:garage:checkpos', (result) => {});
-  mp.events.call('client:player:bank:checkpos');
-  mp.events.callRemote('server:haus:checkpos');
-});
-
-mp.keys.bind(0x71, true, () => {
-  //mp.events.callRemote('server:player:phone:open')
-})
